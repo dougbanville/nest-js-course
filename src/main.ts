8f10c2fa42972ccc57e2b3cd7251a1fe40bb90e2
Doug Banville <doug.banville@gmail.com>
2023-07-31 21:10:35 +0100

    Store stuff with cookies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['dj4239k80jfs0'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -29,7 +29,6 @@ export class AuthService {
     const result = salt + '.' + hash.toString('hex');
 
     //Create a new user and save it
-
     const user = await this.usersService.create(email, result);
     return user;
   }
